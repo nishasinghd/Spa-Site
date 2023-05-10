e50d9e539f868a7af0bbc8d52d4cd2d052b083a4
Nisha ✰ <122490905+nishasinghd@users.noreply.github.com>
2023-05-10 19:40:24 -0400

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', (event) => {
+    new cursoreffects.fairyDustCursor({colors: ["#EFBFEA", "#EFBFEA", "#EFBFEA"]});
+  });
+
+  // function myFunction() {
+  //   var x = document.getElementById("myDIV");
+  //   if (x.style.display === "none") {
+  //     x.style.display = "block";
+  //   } else {
+  //     x.style.display = "none";
+  //   }
+  // }
+
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+
+  
